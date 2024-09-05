661722464a387d052bb21105fb185f1223f54549
Nardy <nardy.allstars@gmail.com>
2024-09-05 17:08:08 +0200

    pokemon data obtained

--- src/app/Details/[name]/page.js
+++ src/app/Details/[name]/page.js
@@ -1,10 +1,38 @@
+"use client";
+
+
+import { useParams } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
 
 function Details() {
 
-  return(
-    <div>asdasd</div>
-  )
-}
+  const { name } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) =>  response.json())
+      .then((data) => {
+        console.log(data);
+        setPokemon(data);
+      })
+     
+      .catch((error) => {
+        console.error('Error fetching Pokémon data:', error);
+        setPokemon(null);
+      });
+  }, [name]);
+
+  if (!pokemon) {
+    return <p>not found</p>;
+  }
+
+  return (
+    <div>
+      <h1>{pokemon.name}</h1>
+
+    </div>
+  );
+};
 
 export default Details
-
