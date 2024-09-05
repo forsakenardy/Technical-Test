4a729bb86f03bce64dfc10de322f683a96cbae36
Nardy <nardy.allstars@gmail.com>
2024-09-05 15:15:29 +0200

    using fetch method for geting the list of pokemons

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,25 @@
+"use client";
+
+import { useState, useEffect } from 'react';
 
 function HomePage() {
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=20')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("pokemon list", data);
+        setPokemons(data.results);
+      })
+      .catch((err) => {
+        console.error("Error fetching Pokémon: ", err);
+      })
+
+  }, []);
+
   return (
     <div ></div>
   );
